a40f2b7ff89b417038f37eedd9e3feef8a26ed2d
tmo macbook <trrl.mo@hotmail.com>
2023-01-02 17:31:36 -0500

    Refactor resident retrieval; calculate avg resident age

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 const axios = require('axios');
 
-class Resident {
-  constructor(residentID) {
-    this.retrieveResident(residentID);
-  }
-
-  retrieveResident = async(residentID) => {
-    const residentEndpoint = // resident endpoint
-
-    try {
-      const response = await axios.get(residentEndpoint);
-      if (response.status == 200) {
-        this.resident = response.data;
-      } else {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-    } catch(e) {
-      // ...
-    }
-  }
-}
-
 class ResidentialBuilding {
   constructor(buildingID) {
-    this.retrieveBuildingAndResidents(buildingID);
+    this.retrieveBuildingAndResidentData(buildingID);
   }
 
-  retrieveBuildingAndResidents = async(buildingID) => {
+  retrieveBuildingAndResidentData = async(buildingID) => {
     try {
       await this.retrieveBuilding(buildingID);
       await this.retrieveResidents();
+      this.averageResidentAge = this.calculateAvgResidentAge();
     } catch(e) {
       console.log(e);
     }
@@ -52,16 +32,40 @@ class ResidentialBuilding {
   }
 
   retrieveResidents = async () => {
+    const residentPromises = this.building.residents.map( async residentID => {
+      const residentEndpoint = // resident endpoint
+
+      const response = axios.get(residentEndpoint);
+      return response;
+    })
+
     try {
-      this.residents = this.building.residents.map( async residentID => {
-        const resident = new Resident(this.building.residents[residentID]);
-        return resident;
-      })
+      this.residents = await Promise.all(residentPromises);
     } catch(e) {
       console.log(e);
     }
 
     console.log(`${this.residents.length} residents retrieved`);
+  }
+
+  calculateAvgResidentAge = () => {
+    let count = 0;
+
+    const sum = this.residents.reduce( (memo, resident, idx) => {
+      if (resident.data) {
+        if (resident.data.age) {
+          count += 1;
+          return memo + resident.data.age;
+        }
+      } else {
+        return memo;
+      }
+    }, 0);
+
+    const avg = Math.round(sum / count);
+    console.log(`Average resident age: ${avg}`);
+
+    this.averageResidentAge = avg;
   }
 }
 
